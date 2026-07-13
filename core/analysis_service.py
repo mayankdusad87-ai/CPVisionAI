ee94d34da7f77951255016e2c137a08e5061f301
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-07-13 20:22:29 +0530

    Update analysis_service.py

--- core/analysis_service.py
+++ core/analysis_service.py
@@ -155,7 +155,7 @@ class AnalysisService:
         partner_df = partner_engine.analyse(df)
 
         partner_analysis = self.partner_analyzer.report(
-        partner_df
+          partner_df
         )
 
         # -------------------------------------------------
