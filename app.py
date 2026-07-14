d3fca216d3d9a38cd0f0b70a545d067f1f0bb9c0
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-07-14 07:31:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,6 +152,17 @@ if selected_page == "🏠 Dashboard":
 
     st.divider()
 
+     st.subheader("🏆 Partner Performance")
+
+    partner_table = result.metadata["partner_table"]
+
+    st.dataframe(
+        partner_table,
+        use_container_width=True,
+        hide_index=True,
+      )
+
+
     
 
     # =====================================================
